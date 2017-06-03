6ea1920e588a3545858df85ec863584e8b1bb8d4
Monalisarah <alles0815@gmx.ch>
2017-06-03 15:52:13 +0200

    finish exercise 1

---
+++ Week07/day01/danceFloor/main.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+function randomColor() {
+  let letters = '0123456789ABCDEF'.split('');
+  let color = '#';
+  for (let i= 0; i<6; i++){
+    color += letters[Math.floor(Math.random()*16)];
+  }
+  return color;
+};
+
+// let interval = setInterval(function () {
+//   let grid = $('.square');
+//   grid.each((index, cell) => {
+//     $(cell).css('background', randomColor());
+//   })
+//
+// }, 5000);
+
+
+// theorie cell und cell acive random dancing:
+let squares = $('.square');
+squares.on('click', (event) => {
+    // ‘active’ greift auf css class zu
+    console.log('in da click')
+    $(event.currentTarget).toggleClass('active');
+});
