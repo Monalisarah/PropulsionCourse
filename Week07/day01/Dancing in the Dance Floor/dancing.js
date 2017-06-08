18cc3f914bf3c5cce43a84189b32259baa6f71f2
Monalisarah <alles0815@gmx.ch>
2017-06-08 08:06:49 +0200

    dance floor exercise

--- Week07/day01/Dancing in the Dance Floor/dancing.js
+++ Week07/day01/Dancing in the Dance Floor/dancing.js
@@ -4,9 +4,8 @@ function grid() {
   for (let i = 0; i < 5; i++) {
     let $row = $("<div class= 'row'>");
     for (let j = 0; j < 5; j++) {
-      let square = $("<div class= 'square'>").attr("id", i+1+ "-" +(j+1))
+      let square = $("<div class= 'square'>").attr("id", i + 1 + "-" + (j + 1))
       $row.append(square)
-
       // $row.append("<div class= 'square'>").attr("id", i+1+ "-" +(j+1) );
     }
     $(".container").append($row);
@@ -24,20 +23,20 @@ function grid() {
 grid();
 
 
-// let selected =
+let selected =
 $(".square").on('click', (event) => {
-    console.log('in da click');
-    $(event.currentTarget).toggleClass('active');
+  console.log('in da click');
+  $(event.currentTarget).toggleClass('active');
 });
 
-// console.log(selected);
+console.log(selected);
 
 
 
 
 
 $(document).on('keypress', (event) => {
-  if (event.key === 'w'){
-    console.log('in da keypress : '+ event.key);
+  if (event.key === 'w') {
+    console.log('in da keypress : ' + event.key);
   }
 });
