17250e1f3d54ca138fbb36b13de108b12701a876
Monalisarah <alles0815@gmx.ch>
2017-06-13 08:09:21 +0200

    redux add todo

--- Week08/day01/Redux Counter/index.js
+++ Week08/day01/Redux Counter/index.js
@@ -1,20 +1,25 @@
 const initialState = {
   counter: 0,
+  todos: ['Learn Redux' , 'Master React']
 }
 
 
 
+
+
 const reducer = function(state = initialState, action) {
-  let newState;
+  let newState = Object.assign({}, state);
   switch (action.type) {
     case "DECREMENT":
-      newState = Object.assign({}, state);
       newState.counter --;
       return newState;
     case "INCREMENT":
-    newState = Object.assign({}, state);
-    newState.counter ++;
-    return newState;
+      newState.counter ++;
+      return newState;
+    case "ADD":
+      const input = document.getElementById("input").value;
+      newState.todos.push(input)
+      input.getAttribute("value") = "";
     default:
       return state;
   }
@@ -25,22 +30,37 @@ const store = Redux.createStore(reducer);
 const render = () => {
   const counter = document.getElementById("counter");
   counter.innerHTML = store.getState().counter;
+  const todo = document.getElementById("todo");
+  todo.innerHTML = "";
+  const todoElements = store.getState().todos;
+    for (let i =0; i < todoElements.length; i++){
+       const list = document.createElement("li")
+       list.innerHTML =  todoElements[i];
+       todo.appendChild(list)
+      }
+
 }
 
 store.subscribe(render);
+render();
 
 
 // action will always be the argument of the dispach
 document
 .getElementById("decrement")
 .addEventListener("click", () => {
-  console.log('click decrement');
   store.dispatch({type: "DECREMENT"})
 });
 
 document
 .getElementById("increment")
 .addEventListener("click", () => {
-  console.log('click increment');
   store.dispatch({type: "INCREMENT"})
+});
+
+document
+.getElementById("add")
+.addEventListener("click", () =>{
+  store.dispatch({type: "ADD"})
+
 });
