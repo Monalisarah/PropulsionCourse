449c936c53dc13cced8e6f1059fbf5965ba11a45
Monalisarah <alles0815@gmx.ch>
2017-06-14 15:04:48 +0200

    add email handler

--- Week08/day03/blitz/src/components/Login/index.js
+++ Week08/day03/blitz/src/components/Login/index.js
@@ -3,11 +3,44 @@ import {connect} from 'react-redux';
 
 
 class Login extends Component {
+
+  constructor(props){
+
+  //it also has access to its parents props
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    }
+  }
+
+handleSubmit = () => {}
+
+handleEmail = (e) => {
+  const email = e.currentTarget.value;
+  console.log(email);
+  this.setState({
+    email: email
+  })
+}
+
+
   render () {
     return(
-      <form>
-        <input type="text" placeholder= "enter your email" name="email"/>
-        <input type="password" placeholder= "password"name="password"/>
+      <form onSubmit={this.handleSubmit}>
+        <br></br>
+        <br></br>
+        <input
+          type="text"
+          placeholder= "enter your email"
+          value = {this.state.email}
+          onChange = {this.handleEmail}/>
+        <input
+          type="password"
+          placeholder= "password"
+          value = {this.state.password}
+          onChange = {this.handlePassword}/>
         <input type="submit" value="Login"/>
       </form>
     );
